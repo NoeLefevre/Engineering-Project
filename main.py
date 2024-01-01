7616b0140ed2702f574594a1dbe0f4f82a86912b
efreims <64196173+efreims@users.noreply.github.com>
2024-01-01 19:56:45 +0100

    Update code

--- main.py
+++ main.py
@@ -3,3 +3,52 @@ Main de l'appli
 Penser à comment le structurer
 S'intéresser d'abord aux modules et ensuite le main de construira petit à petit
 '''
+
+# Imports
+import os
+import sys
+from Scan import reconnaissance
+# Accueil
+print("""
+      
+  _____           _      _     _____           _            _   
+ |  __ \         (_)    | |   |  __ \         | |          | |  
+ | |__) | __ ___  _  ___| |_  | |__) |__ _ __ | |_ ___  ___| |_ 
+ |  ___/ '__/ _ \| |/ _ \ __| |  ___/ _ \ '_ \| __/ _ \/ __| __|
+ | |   | | | (_) | |  __/ |_  | |  |  __/ | | | ||  __/\__ \ |_ 
+ |_|   |_|  \___/| |\___|\__| |_|   \___|_| |_|\__\___||___/\__|
+                _/ |                                            
+               |__/                                             
+
+Par Hugo DAMOIS, Arnaud DUVAL, Noe LEFEVRE, Michael SOK et Sebastien WERNERT      
+""")
+while True:
+    # Saisie adresse IP 
+    ip = input("Saisir l'adresse IP cible : ")
+    print("Adresse IP cible : ", ip)
+
+    while True:
+        print("""
+        Veuillez choisir une option:
+        1. Reconnaissance
+        2. Exploitation
+        3. Elevation de privilèges
+        4. Quitter
+        """)
+
+        choice = input("Veuillez choisir votre option: ")
+
+        if choice == '1':
+            print("Option 1, Reconnaissance, chargement du module...")
+        elif choice == '2':
+            print("Option 2, Exploitation, chargement du module...")
+            
+        elif choice == '3':
+            print("Option 3, Elevation de privilèges, chargement du module...")
+            
+        elif choice == '4':
+            print("Fermeture du programme")
+            break
+        else:
+            print("Choix invalide veuillez recommencer")
+    
